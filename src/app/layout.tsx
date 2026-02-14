7e7c24b8af664102b18aa3a61668965b88f73290
Martin08ABM <martinabm4@pm.me>
2026-02-14 13:55:59 +0100

    feat: update Matomo Analytics integration to use dangerouslySetInnerHTML for script injection

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -130,20 +130,24 @@ export default function RootLayout({
         {children}
 
         {/* Matomo Analytics */}
-        <Script id="matomo-analytics" strategy="afterInteractive">
-          {`
-            var _paq = window._paq = window._paq || [];
-            _paq.push(['trackPageView']);
-            _paq.push(['enableLinkTracking']);
-            (function() {
-              var u="https://optimiseo.matomo.cloud/";
-              _paq.push(['setTrackerUrl', u+'matomo.php']);
-              _paq.push(['setSiteId', '1']);
-              var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-              g.async=true; g.src='https://cdn.matomo.cloud/optimiseo.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
-            })();
-          `}
-        </Script>
+        <Script
+          id="matomo-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              var _paq = window._paq = window._paq || [];
+              _paq.push(['trackPageView']);
+              _paq.push(['enableLinkTracking']);
+              (function() {
+                var u="https://optimiseo.matomo.cloud/";
+                _paq.push(['setTrackerUrl', u+'matomo.php']);
+                _paq.push(['setSiteId', '1']);
+                var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+                g.async=true; g.src='https://cdn.matomo.cloud/optimiseo.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+              })();
+            `,
+          }}
+        />
       </body>
     </html>
   );
